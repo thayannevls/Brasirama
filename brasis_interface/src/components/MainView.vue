5daae5f98c5059156bcc94ae23c98481a326de36
AndresMRM <andres@inventati.org>
2018-08-18 09:06:24 -0300

    restrutura e simplifica

--- brasis_interface/src/components/MainView.vue
+++ brasis_interface/src/components/MainView.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
     <div id="app">
       <lottie :options="defaultOptions" :height="400" :width="400" v-on:animCreated="handleAnimation" />
       <div>
@@ -11,20 +10,21 @@
       <button v-on:click="pause">pause</button>
       <button v-on:click="play">play</button>
     </div>
+    <h2>{{ msg }}</h2>
   </div>
 </template>
 
 <script>
-import Lottie from '../lottie.vue'
+import Lottie from './lottie.vue'
 import * as animationData from '../assets/pinjump.json'
 export default {
-  name: 'HelloWorld',
+  name: 'MainView',
   components: {
     'lottie': Lottie
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Você nasceu em ...',
       defaultOptions: {
         animationData: animationData,
         autoplay: false,
@@ -57,24 +57,5 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1,
-  h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
 </style>
