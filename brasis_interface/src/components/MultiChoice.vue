5487978598616f4af9ef935567ced7c4325afa25
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2018-08-18 11:26:29 -0300

    Adiciona hover aos botões de escolha

--- brasis_interface/src/components/MultiChoice.vue
+++ brasis_interface/src/components/MultiChoice.vue
@@ -2,7 +2,9 @@
   <div>
     <ul>
       <li :key="choice" v-for="choice in choices">
-        {{ choice }}
+        <button :class="{ disabled: wasHovered(choice) }" v-on:mouseover="hover(choice)">
+          {{ choice }}
+        </button>
       </li>
     </ul>
   </div>
@@ -13,16 +15,29 @@ export default {
   name: 'MultiChoice',
   data () {
     return {
-      choices: ['Escola Publica', 'Escola Privada']
+      choices: ['Escola Publica', 'Escola Privada'],
+      hovered: []
     }
   },
   methods: {
+    wasHovered: function (choice) {
+      console.log(choice)
+      return this.hovered.indexOf(choice) !== -1
+    },
     handleAnimation: function (anim) {
       this.anim = anim
+    },
+    hover: function (choice) {
+      if (!this.wasHovered(choice)) {
+        this.hovered.push(choice)
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.disabled{
+  color: red;
+}
 </style>
