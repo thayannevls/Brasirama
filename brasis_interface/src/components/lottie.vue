b55ea75dd39748e7e3f0fcc31c9a951bb2c1e912
AndresMRM <andres@inventati.org>
2018-08-18 14:17:26 -0300

    refac

--- brasis_interface/src/components/lottie.vue
+++ brasis_interface/src/components/lottie.vue
@@ -15,7 +15,6 @@ export default {
     height: Number,
     width: Number
   },
-  
   data () {
     return {
       style: {
@@ -26,7 +25,6 @@ export default {
       }
     }
   },
-  
   mounted () {
     this.anim = lottie.loadAnimation({
       container: this.$refs.lavContainer,
