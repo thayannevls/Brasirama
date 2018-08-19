9e6babec40269655c64ebeda784f0ae741054c14
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2018-08-19 04:15:55 -0300

    Adiciona Multichoice

--- brasis_interface/src/components/MultiChoice.vue
+++ brasis_interface/src/components/MultiChoice.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <ul style="list-style: none;">
-      <li :key="choice" v-for="choice in choices">
-        <button :class="{ disabled: wasHovered(choice), mybutton:!wasHovered(choice) }" v-on:mouseover="hover(choice)">
-          {{ choice }}
+  <div >
+    <div>
+      <div style="display: inline-block">
+        <button>
+          Escola Pública
         </button>
-      </li>
-    </ul>
+        <button :class="{ disabled: wasHovered(choices[0]), mybutton:!wasHovered(choices[0]) }" v-on:mouseover="hover(choices[0])">
+          {{ !wasHovered(choices[0])?'Escola Privada':'Infelizmente você não está nos 25,5% de seu perfil que conseguiram estudo privado' }}
+
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +19,7 @@ export default {
   name: 'MultiChoice',
   data () {
     return {
-      choices: ['Escola Publica', 'Escola Privada'],
+      choices: ['Escola Privada'],
       hovered: []
     }
   },
@@ -36,50 +40,46 @@ export default {
 </script>
 
 <style scoped>
-  * {
-    user-select: none;
-  }
-  .disabled{
-    pointer-events: none;
-    box-shadow:
+* {
+  user-select: none;
+}
+.disabled {
+  pointer-events: none;
+  padding-top:10px;
+  background: #e98155 ;
+  box-shadow:
       0px 0px 0px #fd4d59,0px 0px 0px #fd4d59,
       0px 0px 0px #fd4d59,0px 0px 0px #fd4d59;
-  }
-  button {
-      padding:15px;
-      margin: 0;
-      width:75px;
-      height: 75px;
-      border-radius: 50%;
-      text-transform: uppercase;
-      font-size: 10px;
-      color: #201b53;
-  }
-  ul li{margin-bottom:10px;}
-  button:active {
-    outline: none;
-  }
-  button:focus {
-    outline: 0;
-  }
-  .mybutton {
+}
+button {
+  padding: 15px;
+  margin: 0;
+  width: 40%;
+  text-transform: uppercase;
+  font-size: 14px;
+  background: rgb(255, 163, 143);
+  border: solid 1px rgb(255, 163, 143);
+  box-shadow: 0px 0px 0px #fd4d59, 0px 1px 0px #fd4d59, 0px 1px 0px #fd4d59,
+    0px 2px 0px #fd4d59, 0px 2px 0px #fd4d59, 0px 3px 0px #fd4d59,
+    0px 3px 0px #fd4d59, 0px 4px 0px #fd4d59, 0px 4px 0px #fd4d59,
+    0px 5px 0px #fd4d59, 0px 5px 0px #fd4d59, 0px 6px 0px #fd4d59,
+    0px 6px 0px #fd4d59, 0px 7px 0px #fd4d59, 0px 7px 0px #fd4d59,
+    0px 8px 0px #fd4d59, 0px 8px 0px #fd4d59, 0px 9px 0px #fd4d59;
+}
+ul li {
+  margin-bottom: 10px;
+}
+button:active {
+  outline: none;
+}
+button:focus {
+  outline: 0;
+}
+.mybutton {
 
-    background:rgb(255, 163, 143);
-    border: solid 1px rgb(255, 163, 143);
-    box-shadow:
-        0px 0px 0px #fd4d59, 0px 1px 0px #fd4d59,
-        0px 1px 0px #fd4d59,0px 2px 0px #fd4d59,
-        0px 2px 0px #fd4d59,0px 3px 0px #fd4d59,
-        0px 3px 0px #fd4d59,0px 4px 0px #fd4d59,
-        0px 4px 0px #fd4d59,0px 5px 0px #fd4d59,
-        0px 5px 0px #fd4d59,0px 6px 0px #fd4d59,
-        0px 6px 0px #fd4d59,0px 7px 0px #fd4d59,
-        0px 7px 0px #fd4d59,0px 8px 0px #fd4d59,
-        0px 8px 0px #fd4d59,0px 9px 0px #fd4d59;
-  }
-  .mybutton:hover {
-    box-shadow:
-      0px 0px 0px #fd4d59,0px 0px 0px #fd4d59,
-      0px 0px 0px #fd4d59,0px 0px 0px #fd4d59;
-  }
+}
+.mybutton:hover {
+  box-shadow: 0px 0px 0px #e98155 , 0px 0px 0px #e98155 , 0px 0px 0px #e98155 ,
+    0px 0px 0px #e98155 ;
+}
 </style>
