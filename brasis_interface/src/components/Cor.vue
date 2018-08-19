3f2a6e6e97e1e844e51251beb0835397cef6b842
AndresMRM <andres@inventati.org>
2018-08-19 04:27:50 -0300

    tela 2

---
+++ brasis_interface/src/components/Cor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="/static/image/tela_2_texto.svg" class="image"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cor',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
